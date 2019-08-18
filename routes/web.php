584f169221b5ed154e0b68eea16fbc3ba35abfc6
Joshua Tu <tjh8523@gmail.com>
2019-08-18 15:09:38 +1000

    commented out the fetch data url

--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,16 @@ Route::get('/','FilmsController@index');
 
 Route::get('/film/{id}', 'FilmsController@show');
 
-Route::get('/getFilms', 'FetchData@getFilms');
+// Route::get('/getFilms', 'FetchData@getFilms');
 
-Route::get('/getCharacters', 'FetchData@getCharacters');
+// Route::get('/getCharacters', 'FetchData@getCharacters');
 
-Route::get('/getPlanets', 'FetchData@getPlanets');
+// Route::get('/getPlanets', 'FetchData@getPlanets');
 
-Route::get('/getStarships', 'FetchData@getStarships');
+// Route::get('/getStarships', 'FetchData@getStarships');
 
-Route::get('/getVehicles', 'FetchData@getVehicles');
+// Route::get('/getVehicles', 'FetchData@getVehicles');
 
-Route::get('/getSpecies', 'FetchData@getSpecies');
+// Route::get('/getSpecies', 'FetchData@getSpecies');
 
 
